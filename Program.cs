f5205264de4f944ccea17808814c3bf603501a4e
vitor20022003 <vitorm4123@gmail.com>
2020-11-13 01:57:04 -0300

    algoritmo soma concluido

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("digite o primeiro numero: ");
+            string num1_texto = Console.ReadLine();
+            int num1 = int.Parse(num1_texto);
+
+            Console.Write("digite o segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int soma =num1 + num2;
+
+            Console.Write("o resultado é: ");
+            Console.Write(soma);
+        }
+    }
+    
+}
